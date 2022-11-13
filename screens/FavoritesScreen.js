fd94fb8bcc7cd49c37a52f02488832f2d99e94ab
Sagar Gupta <guptasg384@gmail.com>
2022-11-13 20:49:50 +0530

    first commit

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,30 @@
+import {useContext} from 'react'
+import {Text,View,StyleSheet} from 'react-native'
+import { MEALS } from '../data/dummy-data'
+import { FavoritesContext } from '../store/context/favorites-context'
+
+function FavoritesScreen(){
+    const favoriteMealsCtx = useContext(FavoritesContext)
+    const favoriteMeals = MEALS.filter((meal) => favoriteMealsCtx.ids.includes(meal.id))
+    if(favoriteMeals.length ==0){
+        return <View style={styles.rootContainer}>
+            <Text style={styles.text}>You have no favorite meals yet..</Text>
+        </View>
+    }
+
+}
+
+export default FavoritesScreen
+
+const styles = StyleSheet.create({
+    rootContainer:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    text:{
+        fontSize:18,
+        fontWeight:"bold",
+        color:"white"
+    }
+})
